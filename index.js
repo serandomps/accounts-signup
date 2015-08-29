3b611c6079eaeb030188d4c8d3f541a64aadc2bb
serandules <serandules@gmail.com>
2015-08-29 15:59:01 +0530

    updating

---
+++ index.js
@@ -0,0 +1,47 @@
+var dust = require('dust')();
+var serand = require('serand');
+
+var USERS_API = '/apis/v/users';
+
+dust.loadSource(dust.compile(require('./template'), 'accounts-signup'));
+
+module.exports = function (sandbox, fn, options) {
+    dust.render('accounts-signup', {}, function (err, out) {
+        if (err) {
+            return;
+        }
+        var elem = sandbox.append(out);
+        $('.signup', elem).on('click', function () {
+            $.ajax({
+                url: USERS_API,
+                type: 'POST',
+                headers: {
+                    'x-host': 'accounts.serandives.com'
+                },
+                contentType: 'application/json',
+                data: JSON.stringify({
+                    email: $('.email', elem).val(),
+                    password: $('.password', elem).val()
+                }),
+                dataType: 'json',
+                success: function (data) {
+                    serand.redirect('/signin');
+                },
+                error: function () {
+
+                }
+            });
+        });
+        fn(false, function () {
+            sandbox.remove('.accounts-signup');
+        });
+    });
+};
+
+var user;
+/*
+
+ setTimeout(function () {
+ var serand = require('serand');
+ serand.emit('user', 'login', { username: 'ruchira'});
+ }, 4000);*/
