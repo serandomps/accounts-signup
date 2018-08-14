bde359175af11fcc0746a1a927e1e6c4a17ea24c
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-08-14 09:38:08 +0530

    updating

--- index.js
+++ index.js
@@ -1,37 +1,80 @@
 var dust = require('dust')();
 var serand = require('serand');
 var utils = require('utils');
+var captcha = require('captcha');
 
 var USERS_API = utils.resolve('accounts://apis/v/users');
 
 dust.loadSource(dust.compile(require('./template'), 'accounts-signup'));
 
 module.exports = function (sandbox, options, done) {
-    dust.render('accounts-signup', {}, function (err, out) {
+    var home = options.location || '/';
+    var signin = 'accounts://signin';
+    var suffix = '';
+    var append = function (suff) {
+        suffix += (suffix ? '&' : '?') + suff;
+    }
+    if (options.clientId) {
+        append('client_id=' + options.clientId);
+    }
+    if (options.location) {
+        append('redirect_uri=' + options.location);
+    }
+    signin += suffix;
+    signin = utils.resolve(signin);
+    dust.render('accounts-signup', {home: home, signin: signin}, function (err, out) {
         if (err) {
             return done(err);
         }
-        var elem = sandbox.append(out);
-        $('.signup', elem).on('click', function () {
-            $.ajax({
-                url: USERS_API,
-                type: 'POST',
-                contentType: 'application/json',
-                data: JSON.stringify({
-                    email: $('.email', elem).val(),
-                    password: $('.password', elem).val()
-                }),
-                dataType: 'json',
-                success: function (data) {
-                    serand.redirect('/signin');
-                },
-                error: function (xhr, status, err) {
-                    console.error(err || status || xhr);
+        var captchaId;
+        sandbox.append(out);
+        sandbox.on('click', '.accounts-signup .signup', function () {
+            var el = $('.accounts-signin', sandbox);
+            captcha.response(captchaId, function (err, captcha) {
+                if (err) {
+                    return console.error(err);
                 }
+                if (!captcha) {
+                    return;
+                }
+                $.ajax({
+                    url: USERS_API,
+                    type: 'POST',
+                    contentType: 'application/json',
+                    data: JSON.stringify({
+                        email: $('.email', el).val(),
+                        password: $('.password', el).val()
+                    }),
+                    headers: {
+                        'X-Captcha': captcha
+                    },
+                    dataType: 'json',
+                    success: function (data) {
+                        serand.redirect('/signin');
+                    },
+                    error: function (xhr, status, err) {
+                        console.error(err || status || xhr);
+                    }
+                });
             });
+            return false;
         });
-        done(null, function () {
-            $('.accounts-signup', sandbox).remove();
+        done(null, {
+            clean: function () {
+                $('.accounts-signup', sandbox).remove();
+            },
+            ready: function () {
+                captcha.render($('.captcha', sandbox), {
+                    success: function () {
+                        $('.accounts-signup .signup', sandbox).removeAttr('disabled');
+                    }
+                }, function (err, id) {
+                    if (err) {
+                        return console.error(err);
+                    }
+                    captchaId = id;
+                });
+            }
         });
     });
 };
